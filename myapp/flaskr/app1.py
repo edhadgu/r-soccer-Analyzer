2d5aed90eeaf4b91317a597855597380c9a65775
Elias Hadgu <eliash2@illinois.edu>
2021-12-11 17:46:27 -0600

    UI and edits after testing

--- myapp/flaskr/app1.py
+++ myapp/flaskr/app1.py
@@ -6,6 +6,12 @@ import spacy
 
 reddit = praw.Reddit("user")
 
+def contains_number_or_special(s):
+    special_characters = '"!@#$%^&*()-+?_=,<>/"'
+    for char in s:
+        if char.isdigit() or char in special_characters or char == "'":
+            return True
+    return False
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -36,7 +42,8 @@ def create_app(test_config=None):
         tokenized_corpus = [doc.title.split(" ") for doc in corpus_submissions]
         bm25 = BM25Okapi(tokenized_corpus)
         
-        query = "goal ' - [ ] 0 1 2 3 4 5" # goal submissions are in the format Team A [score] - score Team B - Player ... minute
+        # goal submissions are in the format Team A [score] - score Team B - Player ... minute
+        query = "goal ' - [0] [1] [2] [3] [4] [5] [6] [7]" 
         tokenized_query = query.split(" ")
             
         results = bm25.get_top_n(tokenized_query, corpus_submissions, n=10)
@@ -53,7 +60,7 @@ def create_app(test_config=None):
         bm25 = BM25Okapi(tokenized_corpus)
         bm25_url = BM25Okapi(tokenized_corpus_url)
         
-        query = "FOX ESPN sky twitter athletic"
+        query = "FOX ESPN sky twitter athletic official"
         tokenized_query = query.split(" ")
 
         query_url = "twitter news report daily athletic sky fox espn"
@@ -63,12 +70,11 @@ def create_app(test_config=None):
         results_url = bm25_url.get_top_n(tokenized_query_url, corpus_submissions, n=10)  
         combined_results = results + results_url
         results_no_duplicates = []
-        [results_no_duplicates.append(x) for x in combined_results if x not in results_no_duplicates]
-        return results_no_duplicates
+        [results_no_duplicates.append(x) for x in combined_results if x not in results_no_duplicates and " - " not in x.title and "stream" not in x.url and "gg" not in x.url and  "clip" not in x.url and  "v.reddit" not in x.url]
+        return list(results_no_duplicates)[:10]
 
     # Get list of names in the top posts of the given timeInterval
     # This will include teams and player names
-    # Teams with two words and full names of players will not be shown due to the tokens being split by a space
     def getFanFavorite(timeInterval):
         nlp = spacy.load("en_core_web_sm")
         corpus_submissions = []
@@ -77,18 +83,19 @@ def create_app(test_config=None):
         
         tokenized_corpus = [nlp(doc.title) for doc in corpus_submissions]
 
-        tokenized_corpus_people = []
+        tokenized_corpus_people_or_team = []
         for doc in tokenized_corpus:
             for token in doc.ents:
-                if token.label_ == 'PERSON': # check if each individual word is a person
-                    tokenized_corpus_people.append(token)
+                if token.label_ == 'PERSON' or token.label_ == 'GPE': # check if each individual word is a person or team
+                    tokenized_corpus_people_or_team.append(token)
         
         tokenized_corpus_people_clean = []
         # make sure player names only have characters and remove duplicates
         # Sometimes the 
-        [tokenized_corpus_people_clean.append(str(people)) for people in tokenized_corpus_people if str(people).isalpha() and str(people) not in tokenized_corpus_people_clean]
+        [tokenized_corpus_people_clean.append(str(people)) for people in tokenized_corpus_people_or_team if not contains_number_or_special(str(people)) and str(people) not in tokenized_corpus_people_clean]
 
-        return tokenized_corpus_people_clean
+        return tokenized_corpus_people_clean[:10]
+    
     # Prints out the comments that have the best comment upvote / post upvote ratio
     # Takes very long to loop through comments
     def getBestComments(timeInterval):
@@ -107,28 +114,42 @@ def create_app(test_config=None):
             user_to_score[str(submission.author)] = float(submission.score)
         
         results = dict(sorted(user_to_score.items(), key=lambda item: item[1], reverse=True))
-        return results[:10]
+        return list(results)[:10]
+    # Gets the posts with the highest comment/upvote ratio
+    # Does not include the Match Threads or Discussion posts as those are obvious. 
+    # This function is to find good discussions where the point of the post was not originally discussion
+    def getBestDiscussion(timeInterval):
+        submission_to_score = {}
+        for submission in reddit.subreddit("soccer").top(time_filter=str(timeInterval), limit=100):
+            submission_to_score[submission] = float(submission.num_comments) / float(submission.score)
+        
+        results = dict(sorted(submission_to_score.items(), key=lambda item: item[1], reverse=True))
+        results_without_match_thread = []
+        [results_without_match_thread.append(x) for x in results if "Match Thread" not in x.title and "Discussion" not in x.title]
+        return list(results_without_match_thread)[:10]
 
     @app.route('/')
     def index():
-        return render_template('index.html', data=['best goals of the', 'fan favorites of the', 'top users of the', 'top news of the', 'best comments of the'], timeInterval=['day', 'week', 'month', 'year', 'all'])
+        return render_template('index.html', data=['Best Goals of the', 'Most Popular Topics of the', 'Top Users of the', 'Top Articles of the', 'Best Comments of the', 'Best Discussions of the'], timeInterval=['Day', 'Week', 'Month', 'Year', 'All'])
     
     @ app.route("/test", methods=['GET', 'POST'])
     def test():
         topic=request.form.get('topic_select')
         timeInterval=request.form.get('timeInterval_select')
         
-        if topic == 'best goals of the':
-            return render_template('results.html', data=getBestGoal(timeInterval))
-        elif topic == 'top news of the':
-            return render_template('results.html', data=getTopnews(timeInterval))
-        elif topic == 'fan favorites of the':
-            return render_template('results_strings.html', data=getFanFavorite(timeInterval))
-        elif topic == 'best comments of the':
-            return render_template('results_comments.html', data=getBestComments(timeInterval))
-        elif topic == 'top users of the':
-            return render_template('results_strings_users.html', data=getBestUsers(timeInterval))
+        if topic == 'Best Goals of the':
+            return render_template('results.html', data=getBestGoal(timeInterval.lower()))
+        elif topic == 'Top Articles of the':
+            return render_template('results.html', data=getTopnews(timeInterval.lower()))
+        elif topic == 'Most Popular Topics of the':
+            return render_template('results_strings.html', data=getFanFavorite(timeInterval.lower()))
+        elif topic == 'Best Comments of the':
+            return render_template('results_comments.html', data=getBestComments(timeInterval.lower()))
+        elif topic == 'Top Users of the':
+            return render_template('results_strings_users.html', data=getBestUsers(timeInterval.lower()))
+        elif topic == 'Best Discussions of the':
+            return render_template('results.html', data=getBestDiscussion(timeInterval.lower()))
         
-        return render_template('results.html', data=getBestGoal(timeInterval))
+        return render_template('results.html', data=getBestGoal(timeInterval.lower()))
 
     return app
